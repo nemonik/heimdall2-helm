29ac89770bc7d869e393e8aa8c064798645ab45b
Will Dower <wdower@mitre.org>
2022-07-27 17:24:34 -0400

    pushing to c2il version of chart
    Signed-off-by: Will Dower <wdower@mitre.org>

---
+++ start-heimdall2.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Copyright (C) 2020 Michael Joseph Walsh - All Rights Reserved
+# You may use, distribute and modify this code under the
+# terms of the the license.
+#
+# You should have received a copy of the license with
+# this file. If not, please email <mjwalsh@nemonik.com>
+
+# secrets
+DATABASE_USERNAME="postgres"
+DATABASE_PASSWORD="$(openssl rand -hex 33)"
+JWT_SECRET="$(openssl rand -hex 64)"
+JWT_TIMEOUT="1d"
+
+# Install heimdall
+helm install heimdall . -n heimdall \
+     -f values.yaml \
+    --set   databaseUsername=$DATABASE_USERNAME \
+    --set   databasePassword=$DATABASE_PASSWORD \
+    --set   jwtSecret=$JWT_SECRET \
+    --set   jwtTimeout=$JWT_TIMEOUT 
